e118ea8bf99068a484b41c4db9dae35494b9b9d6
Skyler Bexten <skyler.bexten@gmail.com>
2025-08-26 21:00:07 -0700

    Adjust style again

--- src/components/SourceDisplay.tsx
+++ src/components/SourceDisplay.tsx
@@ -6,37 +6,36 @@ type SourceDisplayProps = {
     source: Source;
 };
 
-export const SourceDisplay: React.FC<SourceDisplayProps> = ({ source }) => (
-    <div>
-        <h3>{source.name} Source</h3>
-        <ul style={{ border: "1px solid #ccc", borderRadius: "6px", padding: "1rem", margin: 0, listStyle: "none" }}>
-            {source.verses.map((v, i) => {
-                let background;
-                switch (source.name) {
-                    case "P":
-                        background = colors.sourceP;
-                        break;
-                    case "J":
-                        background = colors.sourceJ;
-                        break;
-                    case "E":
-                        background = colors.sourceE;
-                        break;
-                    default:
-                        background = undefined;
-                }
-                return (
+export const SourceDisplay: React.FC<SourceDisplayProps> = ({ source }) => {
+    let background;
+    switch (source.name) {
+        case "P":
+            background = colors.sourceP;
+            break;
+        case "J":
+            background = colors.sourceJ;
+            break;
+        case "E":
+            background = colors.sourceE;
+            break;
+        default:
+            background = undefined;
+    }
+    return (
+        <div>
+            <h3>{source.name} Source</h3>
+            <ul style={{ background, border: "1px solid #ccc", borderRadius: "6px", padding: "1rem", margin: 0, listStyle: "none" }}>
+                {source.verses.map((v, i) => (
                     <li
                         key={i}
                         style={{
-                            ...(background ? { background } : {}),
                             marginBottom: "0.5em"
                         }}
                     >
                         <strong>{v.chapter}:{v.verse}</strong> {v.text}
                     </li>
-                );
-            })}
-        </ul>
-    </div>
-);
+                ))}
+            </ul>
+        </div>
+    );
+};
