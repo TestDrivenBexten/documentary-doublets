0f34a67e995297a9fafd45d14149c222afd5c446
Skyler Bexten <skyler.bexten@gmail.com>
2025-09-13 17:15:44 -0700

    Add horizontal and vertical stacking

--- src/components/DoubletDisplay.tsx
+++ src/components/DoubletDisplay.tsx
@@ -10,6 +10,7 @@ type DoubletDisplayProps = {
 };
 
 export const DoubletDisplay: React.FC<DoubletDisplayProps> = ({ doublet }) => {
+    const [horizontal, setHorizontal] = useState(false);
     const sourceNames = doublet.sources?.map(s => s.name) || [];
     const sortedSourceNames = sortSourceNames(sourceNames);
     const [filter, setFilter] = useState<SourceName[]>(sortedSourceNames);
@@ -23,16 +24,43 @@ export const DoubletDisplay: React.FC<DoubletDisplayProps> = ({ doublet }) => {
             <h2>{doublet.title}</h2>
             {doublet.sources && (
                 <>
-                    <SourceDisplayFilter
-                        options={sortedSourceNames}
-                        selected={filter}
-                        onChange={setFilter}
-                    />
-                    <div style={{ display: "flex", gap: "2rem" }}>
+                    <div
+                        style={{
+                            display: "flex",
+                            gap: "1em"
+                        }}
+                    >
+                        <SourceDisplayFilter
+                            options={sortedSourceNames}
+                            selected={filter}
+                            onChange={setFilter}
+                        />
+                        <span
+                            style={{
+                                cursor: "pointer",
+                                fontSize: "1.5em",
+                                display: "inline-block"
+                            }}
+                            title={`Stack ${horizontal ? "Vertically" : "Horizontally"}`}
+                            onClick={() => setHorizontal(h => !h)}
+                        >
+                            {horizontal ? "↔" : "↕"}
+                        </span>
+                    </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: horizontal ? "row" : "column",
+                            gap: "1em"
+                        }}
+                    >
                         {sortSourcesByName(doublet.sources)
                             .filter(source => filter.includes(source.name))
                             .map((source, idx) => (
-                                <SourceDisplay key={idx} source={source} />
+                                <SourceDisplay
+                                    key={idx}
+                                    source={source}
+                                />
                             ))}
                     </div>
                 </>
